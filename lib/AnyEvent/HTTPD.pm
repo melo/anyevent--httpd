201f69bd1725e111bf561449e2bdc125419173d9
Robin Redeker <elmex@ta-sa.org>
2008-04-16 12:49:55 +0200

    bumped version

--- lib/AnyEvent/HTTPD.pm
+++ lib/AnyEvent/HTTPD.pm
@@ -16,11 +16,11 @@ AnyEvent::HTTPD - A simple lightweight event based web (application) server
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
